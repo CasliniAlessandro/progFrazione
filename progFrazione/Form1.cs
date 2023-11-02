1a775f2565c9ddba3046d8ccf66f78c291922908
caslini.20353 <caslini.20353@E2-9-PC20>
2023-11-02 10:54:02 +0100

    somma sottrazione moltiplicazione divisione button e listiview

--- progFrazione/Form1.cs
+++ progFrazione/Form1.cs
@@ -12,14 +12,53 @@ namespace progFrazione
 {
     public partial class Form1 : Form
     {
+        private Frazione frazione1;
+        private Frazione frazione2; 
         public Form1()
         {
             InitializeComponent();
+            frazione1 = new Frazione(1, 2);
+            frazione2=new Frazione(2,3);    
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frazione1.semplificaFrazione();
+            frazione2.semplificaFrazione();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Frazione result = frazione1.Somma(frazione2);
+            result.semplificaFrazione();
+            listView1.Items.Add("somma"+ result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Frazione result =frazione1.Sottrazione(frazione2);
+            result.semplificaFrazione();
+            listView1.Items.Add("differenza" + result);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Frazione result = frazione1.Moltiplicazione(frazione2);
+            result.semplificaFrazione();
+            listView1.Items.Add("prodotto" + result);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Frazione result = frazione1.Dividi(frazione2);
+            result.semplificaFrazione();
+            listView1.Items.Add("divisione" + result);
         }
     }
 }
--- progFrazione/Frazione.cs
+++ progFrazione/Frazione.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace progFrazione
 {
-    internal class Frazione
+    class Frazione
     {
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+
+
+        public Frazione(int numeratore,int denominatore)  
+        {
+            Numeratore= numeratore;
+            Denominatore= denominatore;
+        }
+
+
+        public Frazione(Frazione altraFr)
+        {
+            Numeratore= altraFr.Numeratore;
+            Denominatore = altraFr.Denominatore;
+        }
+         
+        public void semplificaFrazione()
+        {
+            int mcd=MCD(Numeratore,Denominatore);
+            Numeratore/= mcd;
+            Denominatore/= mcd;  
+        }
+        public Frazione Somma(Frazione altraFr)
+        {
+            int NuovoNumeratore= Numeratore*altraFr.Denominatore+altraFr.Numeratore*Denominatore;
+            int NuovoDenominatore = Denominatore * altraFr.Numeratore + altraFr.Denominatore * Numeratore;
+            return new Frazione(NuovoNumeratore, NuovoDenominatore);
+        }
+
+        public Frazione Sottrazione(Frazione altraFr)
+        {
+            int NuovoNumeratore = Numeratore * altraFr.Denominatore - altraFr.Numeratore * Denominatore;
+            int NuovoDenominatore = Denominatore * altraFr.Numeratore - altraFr.Denominatore * Numeratore;
+            return new Frazione(NuovoNumeratore, NuovoDenominatore);
+        }
+    }
     }
 }
